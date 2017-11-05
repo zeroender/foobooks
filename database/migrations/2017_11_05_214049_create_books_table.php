51eac9d726c1cd7f96a84f46ed5ecacaebfb9eac
Brian Worthington <worthington.brian@gmail.com>
2017-11-05 16:47:36 -0500

    Adding migration to foobooks

---
+++ database/migrations/2017_11_05_214049_create_books_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBooksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+            # Increments method will make a Primary, Auto-Incrementing field.
+            # Most tables start off this way
+            $table->increments('id');
+
+            # This generates two columns: `created_at` and `updated_at` to
+            # keep track of changes to a row
+            $table->timestamps();
+
+            # The rest of the fields...
+            $table->string('title');
+            $table->string('author')->nullable(); # Example of a modifier
+            $table->integer('published');
+            $table->string('cover');
+            $table->string('purchase_link');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('books');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,33 @@
 <?php
 
+//debug route added in week 10 for testing database
+Route::get('/debug', function () {
+
+    $debug = [
+        'Environment' => App::environment(),
+        'Database defaultStringLength' => Illuminate\Database\Schema\Builder::$defaultStringLength,
+    ];
+
+    /*
+    The following commented out line will print your MySQL credentials.
+    Uncomment this line only if you're facing difficulties connecting to the
+    database and you need to confirm your credentials. When you're done
+    debugging, comment it back out so you don't accidentally leave it
+    running on your production server, making your credentials public.
+    */
+    #$debug['MySQL connection config'] = config('database.connections.mysql');
+
+    try {
+        $databases = DB::select('SHOW DATABASES;');
+        $debug['Database connection test'] = 'PASSED';
+        $debug['Databases'] = array_column($databases, 'Database');
+    } catch (Exception $e) {
+        $debug['Database connection test'] = 'FAILED: '.$e->getMessage();
+    }
+
+    dump($debug);
+});
+
 /**
 * Code from Week 7 progress log
 */
